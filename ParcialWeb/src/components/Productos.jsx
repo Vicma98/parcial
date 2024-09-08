ed0c71a3d3f7524089b8c6d151dec5d78134a840
Vicma98 <vhernandez@unac.edu.co>
2024-09-08 17:54:00 -0500

    punto2 en productos

--- ParcialWeb/src/components/Productos.jsx
+++ ParcialWeb/src/components/Productos.jsx
@@ -24,3 +24,34 @@ export function Productos() {
         </div>
     );
 }
+
+function sumarCuadros(arrayNumeros) {
+    let total = 0;
+    arrayNumeros.forEach((numero) => {
+
+        const numString = numero.toString();
+        const longitud = numString.length;
+        
+        console.log("+ " + "-".repeat(longitud) + " +");
+        
+        console.log("| " + numString + " |");
+        
+      //se suma el numero al total
+        total += numero;
+    });
+    
+    const totalString = total.toString();
+    const longitudTotal = totalString.length;
+    
+    
+    console.log("+ " + "-".repeat(longitudTotal) + " +");
+    
+    console.log("| " + totalString + " |");
+    
+    
+    console.log("+ " + "-".repeat(longitudTotal) + " +");
+}
+
+  // Ejemplo
+const arrayNumeros = [1, 23, 453, 3267, 12354, 123456];
+sumarCuadros(arrayNumeros);
