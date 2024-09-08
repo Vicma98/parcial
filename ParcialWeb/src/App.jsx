d8a87a8f92cfe575e8fb0968ab8fdb58c01ecd86
Vicma98 <vhernandez@unac.edu.co>
2024-09-08 12:36:31 -0500

    first commit

---
+++ ParcialWeb/src/App.jsx
@@ -0,0 +1,12 @@
+import { Formulario } from './components/formulario'
+import './App.css'
+
+function App() {
+  return (
+      <div className='App'>
+        <Formulario/>
+      </div>
+  )
+}
+
+export default App
---
+++ ParcialWeb/src/components/Formulario.jsx
@@ -0,0 +1,15 @@
+import "../components/Formulario.css"
+export function Formulario(){
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form className="formulario">
+                <input type="text" />
+                <input type="password" />
+                <button >LOGIN</button>
+            </form>
+
+        </div>
+    )
+}
